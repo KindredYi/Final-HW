c68e148fc04a4a22b8ff56aafe1220f3a8acb8da
Kindred_Yi <775254646@qq.com>
2021-07-01 17:49:44 +0800

    1

---
+++ mask_detect/utils/k_means.py
@@ -0,0 +1,72 @@
+
+from PIL import Image
+import numpy as np
+
+
+def iou(box,cluster):
+    x=np.minimum(cluster[:,0],box[0])
+    y=np.minimum(cluster[:,1],box[1])
+    i=x*y
+    area1=box[0]*box[1]
+    arer2=cluster[:,0]*cluster[:,1]
+    IoU=i/(area1+arer2-i)
+    return IoU
+
+def kmeans(box,k):
+    box_num=box.shape[0]
+    distance=np.empty((box_num,k))
+    clu=np.zeros((box_num,))
+    np.random.seed()
+    cluster=box[np.random.choice(box_num,k,replace=False)]
+    while True:
+        for i in range(box_num):
+            distance[i]=1-iou(box[i],cluster)
+        nearst=np.argmin(distance,axis=1)
+        if (clu==nearst).all():
+            break
+        for j in range(k):
+            cluster[j]=np.median(box[nearst==j],axis=0)
+        clu=nearst
+    return cluster
+
+def read_txt(path):
+    data=[]
+    f=open(path)
+    lines=f.readlines()
+    for line in lines:
+        line=line.strip()
+        imgpath=line[:52]
+        img=Image.open(imgpath)
+        w,h=int(img.size[0]),int(img.size[1])
+        boxes=line[53:]
+        boxes_format=boxes.split()
+        for i in boxes_format:
+            box=i.split(',')
+            print(i)
+            xmin=np.float64(int(box[0])/w)
+            ymin=np.float64(int(box[1])/h)
+            xmax=np.float64(int(box[2])/w)
+            ymax=np.float64(int(box[3])/h)
+            data.append([xmax-xmin,ymax-ymin])
+    return np.array(data)
+
+
+
+if __name__=='__main__':
+    txtpath=r'../logs/trainlist_20000.txt'
+    size=416
+    anc_num=6
+    hwdata=read_txt(txtpath)
+    out_anchor=kmeans(hwdata,anc_num)
+    out_anchor=out_anchor[np.argsort(out_anchor[:,0])]
+    hwdata=out_anchor*416
+    f=open('../logs/kmeans_anchor.txt', 'w')
+    num=np.shape(hwdata)[0]
+    for i in range(num):
+        if i==0:
+            x_y="%d,%d"%(hwdata[i][0],hwdata[i][1])
+        else:
+            x_y=", %d,%d"%(hwdata[i][0],hwdata[i][1])
+        f.write(x_y)
+    f.close()
+    print('已生成先验框')
--- mask_detect/utils/utils.py
+++ mask_detect/utils/utils.py
@@ -18,8 +18,6 @@ def bbox_iou(box1, box2):
     iou = inter_area / (b1_area + b2_area - inter_area + 1e-16)
     return iou
 
-
-
 class DecodeBox(nn.Module):
     def __init__(self, anchors, num_classes, img_size):
         super(DecodeBox, self).__init__()
@@ -30,12 +28,15 @@ class DecodeBox(nn.Module):
         self.img_size = img_size
 
     def forward(self, input):
+        print('当前特征层:{}'.format(input.shape))
         batch_size = input.size(0)
         input_height = input.size(2)
         input_width = input.size(3)
         stride_h = self.img_size[1] / input_height
         stride_w = self.img_size[0] / input_width
+        print('缩放因数:{}'.format(stride_w))
         scaled_anchors = [(anchor_width / stride_w, anchor_height / stride_h) for anchor_width, anchor_height in self.anchors]
+        print(scaled_anchors)
         prediction = input.view(batch_size, self.num_anchors,
                                 self.bbox_attrs, input_height, input_width).permute(0, 1, 3, 4, 2).contiguous()
         x = torch.sigmoid(prediction[..., 0])
@@ -65,8 +66,14 @@ class DecodeBox(nn.Module):
         pred_boxes[..., 3] = torch.exp(h.data) * anchor_h
 
         scale = torch.Tensor([stride_w, stride_h] * 2).type(FloatTensor)
+        print('scale={}'.format(scale))
         output = torch.cat((pred_boxes.view(batch_size, -1, 4) * scale,
                             conf.view(batch_size, -1, 1), pred_cls.view(batch_size, -1, self.num_classes)), -1)
+        print('pred_box:{}'.format(pred_boxes.shape))
+        print('viewed_pred_box:{}'.format((pred_boxes.view(batch_size,-1,4)*scale).shape))
+        print('conf:{}'.format(conf.view(batch_size, -1, 1).shape))
+        print('pred_cls:{}'.format(pred_cls.view(batch_size, -1, self.num_classes).shape))
+        print('解码器输出:{}'.format(output.shape))
         return output.data
 
 def letterbox(image, size):
@@ -80,28 +87,37 @@ def letterbox(image, size):
     new_image.paste(image, ((w-nw)//2, (h-nh)//2))
     return new_image
 
-def NMS(prediction,conf_thres=0.8,iou_thres=0.13):
+def NMS(prediction,conf_thres=0.8,iou_thres=0.25):
     box_temp=prediction.new(prediction.shape)
     box_temp[:,:,0]=prediction[:,:,0]-prediction[:,:,2]/2
     box_temp[:,:,1]=prediction[:,:,1]-prediction[:,:,3]/2
     box_temp[:,:,2]=prediction[:,:,0]+prediction[:,:,2]/2
     box_temp[:,:,3]=prediction[:,:,1]+prediction[:,:,3]/2
     prediction[:,:,:4]=box_temp[:,:,:4]
+    print('NMS 输入:{}'.format(prediction.shape))
 
     output = [None for _ in range(len(prediction))]
 
     for index,pred in enumerate(prediction):
+        print('共{}个边界框'.format(len(pred)))
         cls_conf,cls_pred=torch.max(pred[:,5:7],dim=1,keepdim=True)
         score=pred[:,4]*cls_conf[:,0]
+        size_a=pred.shape[0]
         pred=pred[(score>conf_thres).squeeze()]
+        size_b=pred.shape[0]
+        print("根据置信度得分删除{}个预测框，还剩{}个预测框".format(size_a-size_b,size_b))
         cls_conf=cls_conf[(score>conf_thres).squeeze()]
         cls_pred=cls_pred[(score>conf_thres).squeeze()]
         if not pred.size(0):
             continue
         detection=torch.cat((pred[:,:5],cls_conf.float(),cls_pred.float()),dim=1)
+        print(detection.shape)
         all_cls=detection[:,-1].cpu().unique()
         for cls in all_cls:
+            print('*****'*10)
+            print('class_{}'.format(cls))
             is_cls=detection[:,-1]==cls
+            print(is_cls)
             detection_class=detection[is_cls]
             boxes=detection_class[:,:4]
             score=detection_class[:,4]*detection_class[:,5]
@@ -109,6 +125,9 @@ def NMS(prediction,conf_thres=0.8,iou_thres=0.13):
             max_detection=detection_class[keep]
             output[index] = max_detection if output[index] is None else torch.cat(
                 (output[index], max_detection))
+        for i in output:
+            print('NMS输出:{}'.format(i.shape))
+        print('NMS删除{}个框'.format(size_b-len(output)))
 
     return output
 
@@ -130,3 +149,62 @@ def remove_gray(top, left, bottom, right, input_shape, image_shape):
     ],axis=-1)
     boxes *= np.concatenate([image_shape, image_shape],axis=-1)
     return boxes
+
+
+def merge_bboxes(bboxes, cutx, cuty):
+    merge_bbox = []
+    for i in range(len(bboxes)):
+        for box in bboxes[i]:
+            tmp_box = []
+            x1, y1, x2, y2 = box[0], box[1], box[2], box[3]
+            if i == 0:
+                if y1 > cuty or x1 > cutx:
+                    continue
+                if y2 >= cuty and y1 <= cuty:
+                    y2 = cuty
+                    if y2 - y1 < 5:
+                        continue
+                if x2 >= cutx and x1 <= cutx:
+                    x2 = cutx
+                    if x2 - x1 < 5:
+                        continue
+            if i == 1:
+                if y2 < cuty or x1 > cutx:
+                    continue
+                if y2 >= cuty and y1 <= cuty:
+                    y1 = cuty
+                    if y2 - y1 < 5:
+                        continue
+                if x2 >= cutx and x1 <= cutx:
+                    x2 = cutx
+                    if x2 - x1 < 5:
+                        continue
+            if i == 2:
+                if y2 < cuty or x2 < cutx:
+                    continue
+                if y2 >= cuty and y1 <= cuty:
+                    y1 = cuty
+                    if y2 - y1 < 5:
+                        continue
+                if x2 >= cutx and x1 <= cutx:
+                    x1 = cutx
+                    if x2 - x1 < 5:
+                        continue
+            if i == 3:
+                if y1 > cuty or x2 < cutx:
+                    continue
+                if y2 >= cuty and y1 <= cuty:
+                    y2 = cuty
+                    if y2 - y1 < 5:
+                        continue
+                if x2 >= cutx and x1 <= cutx:
+                    x1 = cutx
+                    if x2 - x1 < 5:
+                        continue
+            tmp_box.append(x1)
+            tmp_box.append(y1)
+            tmp_box.append(x2)
+            tmp_box.append(y2)
+            tmp_box.append(box[-1])
+            merge_bbox.append(tmp_box)
+    return merge_bbox
